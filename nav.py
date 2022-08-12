c7524a0f4ffce53a2a2344b775712f1f510d6500
rando-brando <111151396+rando-brando@users.noreply.github.com>
2022-08-12 15:44:28 -0400

    lazy manual upload

---
+++ nav.py
@@ -0,0 +1,23 @@
+#%%
+import pandas as pd
+import yahooquery as yq
+
+#%%
+def getPrice(symbol):
+    try:
+        price = yq.Ticker(symbol).price[symbol]['regularMarketPrice']
+    except:
+        price = None
+    return price
+
+#%%
+funds = pd.read_json("funds.json")
+
+#%%
+funds['price'] = funds['symbol'].apply(getPrice)
+funds['nav'] = funds['nav'].apply(getPrice)
+funds['premium'] = 100 * (funds['price'] - funds['nav']) / funds['nav']
+
+#%%
+funds.to_csv("NAV.csv")
+# %%
