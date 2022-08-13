ca5f759202d5816643fd2e3b0498a490407dde47
brandon rich <brandon.rich.c@gmail.com>
2022-08-12 20:50:10 -0400

    added plotly figure to output

--- nav.py
+++ nav.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd
 import yahooquery as yq
+import plotly.express as px
 
 #%%
 def getPrice(symbol):
@@ -17,6 +18,12 @@ funds = pd.read_json("funds.json")
 funds['price'] = funds['symbol'].apply(getPrice)
 funds['nav'] = funds['nav'].apply(getPrice)
 funds['premium'] = 100 * (funds['price'] - funds['nav']) / funds['nav']
+funds.sort_values(by = "premium", inplace = True)
 
 #%%
 funds.to_csv("NAV.csv", index = False)
+
+#%%
+plt = px.bar(funds, x = 'symbol', y = 'premium', labels = {'premium': 'discount/premium'}, height = 800)
+plt.update_layout(title_text = 'Discount or Premium to NAV', title_x = 0.5).update_yaxes(dtick = 5).show()
+# %%
